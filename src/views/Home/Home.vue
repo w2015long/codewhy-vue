f86219b34c78df58458d3427c1758e2779e14e21
Jsen <w2015long@qq.com>
2020-05-08 21:09:01 +0800

    components of api

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -3,6 +3,11 @@
 </template>
 
 <script>
+    //接口 http://123.207.32.32:8000/home/multidata
+    //get 请求
+    // type: sell / pop  String  N非必传
+    //page: 1            Int     N非必传
+
     export default {
         name: "Home"
     }
